bb9d90164ba41b8ff0bfb50543768c002fd978e1
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-02-11 19:45:25 +0530

    header styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import './App.css';
+import "./App.css";
+import useLocalStorage from "use-local-storage";
+import light_mode from './media/light-mode.png';
+import dark_mode from './media/night-mode.png';
 
 function App() {
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
   return (
-    <div className="App">
+    <div className="App" data-theme={theme}>
+      <header>
+        <h3>Where in the world?</h3>
+        <button onClick={switchTheme}>
+          <img src={theme === "light" ? dark_mode : light_mode} />
+          {theme === "light" ? "Dark" : "Light"} Mode
+        </button>
+      </header>
     </div>
   );
 }
