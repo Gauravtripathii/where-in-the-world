2c7b87a2874cce2bb56420239196fa91b82b3a43
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-02-13 18:50:29 +0530

    cuntry-detail responsive

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -9,33 +9,35 @@ function CountryDetail() {
       <div className="flag">
         <img src={from.flags.png} alt="flag" />
       </div>
-      <h3>{from.name}</h3>
-      <ul>
-        <li>
-          <span>native name: </span>
-          {from.nativeName}
-        </li>
-        <li>
-          <span>population: </span>
-          {from.population}
-        </li>
-        <li>
-          <span>region: </span>
-          {from.region}
-        </li>
-        <li>
-          <span>sub region: </span>
-          {from.subregion}
-        </li>
-        <li>
-          <span>capital: </span>
-          {from.capital}
-        </li>
-        <li>
-          <span>top level domain: </span>
-          {from.topLevelDomain}
-        </li>
-      </ul>
+      <div className="right">
+        <h3>{from.name}</h3>
+        <ul>
+          <li>
+            <span>native name: </span>
+            {from.nativeName}
+          </li>
+          <li>
+            <span>population: </span>
+            {from.population}
+          </li>
+          <li>
+            <span>region: </span>
+            {from.region}
+          </li>
+          <li>
+            <span>sub region: </span>
+            {from.subregion}
+          </li>
+          <li>
+            <span>capital: </span>
+            {from.capital}
+          </li>
+          <li>
+            <span>top level domain: </span>
+            {from.topLevelDomain}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
